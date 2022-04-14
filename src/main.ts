822151869a799c55d0d2232563216e9b7be26e75
Nikolay Lapshin <nlapshin1989@gmail.com>
2022-04-14 16:57:35 +0300

    Added interceptor

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './http-exception-filter';
+import { LoggingInterceptor } from './logger.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new LoggingInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(new ValidationPipe());
   // app.useGlobalGuards(new AutGuard());
